2b31165768c4f5a511e0cf01f82015009ab1f8d5
Bonko24 <134968235+Bonko24@users.noreply.github.com>
2023-06-08 15:28:20 -0600

    Add files via upload

--- Proyecto Riteve.py
+++ Proyecto Riteve.py
@@ -18,6 +18,8 @@ reteve_principal = Tk()
 cita = StringVar()
 num_cita = datos[0]
 
+elegida = IntVar()
+
 #?============================================================= Secundarias ===================================================================================================
 def programar_cita():
     p_citas = Toplevel()
@@ -34,77 +36,87 @@ def programar_cita():
         for child in frame_scrollbar.winfo_children():
             child.configure(state="disabled")
 
-<<<<<<< HEAD
-
-=======
     # TODO cambio grande: reacomodé todo para que quepa
->>>>>>> 1eb4ae5 (Versión 1.1)
 
     #!Label titulo
-    config_cita = Label(p_citas, relief = 'solid',  text ='Programe su cita', font=('Times New Roman', 32), width = 15)
+    config_cita = Label(p_citas, relief = 'solid',  text ='Programe su cita', font=('Times New Roman', 20), width = 15)
     config_cita.place(x=10,y=10)
     
     #!Label numero de cita
-<<<<<<< HEAD
-    etiqueta1 = Label(p_citas, text = "Número de cita:", font = ("Times New Roman", 13, "bold"))
-    etiqueta1.place(x = 10, y = 80)
-    cita = Label(p_citas, text = str(num_cita + 1 ), font = ("Times New Roman", 13, "bold"),fg='blue')
-=======
-    etiqueta1 = Label(p_citas, text = "Número de cita", font = ("Times New Roman", 13, "bold"))
-    etiqueta1.place(x = 10, y = 80)
+    tit_cita = Label(p_citas, text = "Número de cita:", font = ("Times New Roman", 13, "bold"))
+    tit_cita.place(x = 10, y = 80)
     cita = Label(p_citas, text = str(num_cita), font = ("Times New Roman", 13, "bold"),fg='blue')
->>>>>>> 1eb4ae5 (Versión 1.1)
-    cita.place(x = 150, y = 80)
+    cita.place(x = 250, y = 80)
 
     #>! PRUEBA
     def valor():
         print(tipo_revision.get())
+        
+    #! RESTRICCIONES Y VALIDACIONES DE DATOS
+    def long_placa(evento):
+        entry_placa = placa.get()
+        if len(entry_placa) > 8:
+            placa_valida = entry_placa[:8]
+            placa.delete(0, END)
+            placa.insert(0, placa_valida)
+    
+    def long_marca(evento):
+        entry_marca = marca.get()
+        if len(entry_marca) > 15:
+            marca_valida = entry_marca[:15]
+            marca.delete(0, END)
+            marca.insert(0, marca_valida)
+            
+    def long_modelo(evento):
+        entry_modelo = modelo.get()
+        if len(entry_modelo) > 15:
+            modelo_valido = entry_modelo[:15]
+            modelo.delete(0, END)
+            modelo.insert(0, modelo_valido)
+            
+    def long_prop(evento):
+        entry_propietario = propietario.get()
+        if len(entry_propietario) > 40:
+            prop_valida = entry_propietario[:40]
+            propietario.delete(0, END)
+            propietario.insert(0, prop_valida)
+            
+    def long_telefono(evento):
+        entry_telefono = telefono.get()
+        if len(entry_telefono) > 20:
+            telefono_valida = entry_telefono[:20]
+            telefono.delete(0, END)
+            telefono.insert(0, telefono_valida)
+            
+    def long_direc(evento):
+        entry_direccion = direccion.get()
+        if len(entry_direccion) > 40:
+            direccion_valida = entry_direccion[:40]
+            direccion.delete(0, END)
+            direccion.insert(0, direccion_valida)
 
     #? Check box y su label
-    tipo_lab = Label(p_citas, text= 'Elija su tipo de cita', font = ("Times New Roman", 13, "bold"))
-    tipo_lab.place(x = 10, y = 140)
+    tipo_lab = Label(p_citas, text= 'Elija su tipo de cita:', font = ("Times New Roman", 13, "bold"))
+    tipo_lab.place(x = 10, y = 130)
 
-<<<<<<< HEAD
-    p_rev = Checkbutton(p_citas, text = "Primera vez", variable = tipo_revision, onvalue = 1, offvalue = 0, height=5,width = 20,command=valor)
-    p_rev.place(x = 10, y = 160)
-    reins = Checkbutton(p_citas, text = "Reinspección", variable = tipo_revision, onvalue = 2, offvalue = 0, height=5, width = 20,command=valor)
-    reins.place(x = 140, y = 160)
-
-    #? Placa y tit
-    tit_placa = Label(p_citas, text= 'Ingrese su numero de placa', font = ("Times New Roman", 13, "bold"))
-    tit_placa.place(x = 10, y = 240)
-    placa = Entry(p_citas,font=('Times New Roman', 18), width = 35,textvariable=num_placa)
-    placa.place(x = 10, y = 260)
-    
-    #? Tipo Vehiculo titulo
-    tit_tcarro = Label(p_citas, text= 'Tipo de Vehiculo', font = ("Times New Roman", 13, "bold"))
-    tit_tcarro.place(x = 10, y = 300)
-    frame_vehiculo = Frame(p_citas)
-    frame_vehiculo.place(x = 10, y = 340, width=100, height=50)
-    vehiculos_list = StringVar()
-    vehiculos_list.set(["Vehiculo Especial","Vehiculo Ligero","Vehiculo Pesado"])
-    listbox_vehiculo = Listbox(frame_vehiculo,listvariable=vehiculos_list)
-    listbox_vehiculo.pack()
-
-    #? Marca de Vehiculo
-
-    #! Scroll bar 
-=======
     p_rev = Checkbutton(p_citas, text = "Primera vez", variable = tipo_revision, onvalue = 1, offvalue = 0, height=5,width = 20,command=valor, font = ("Times New Roman", 13))
-    p_rev.place(x = 160, y = 100)
+    p_rev.place(x = 280, y = 90)
     reins = Checkbutton(p_citas, text = "Reinspección", variable = tipo_revision, onvalue = 2, offvalue = 0, height=5, width = 20,command=valor, font = ("Times New Roman", 13))
-    reins.place(x = 320, y = 100)
+    reins.place(x = 440, y = 90)
 
     #? Placa y tit
-    tit_placa = Label(p_citas, text = 'Número de placa', font = ("Times New Roman", 13, "bold"))
-    tit_placa.place(x = 10, y = 200)
-    placa = Entry(p_citas,font=('Times New Roman', 13), width = 35,textvariable = num_placa)
-    placa.place(x = 160, y = 200)
+    tit_placa = Label(p_citas, text = 'Número de placa:', font = ("Times New Roman", 13, "bold"))
+    tit_placa.place(x = 10, y = 180)
+    placa = Entry(p_citas,font=('Times New Roman', 13), width = 8,textvariable = num_placa)
+    placa.place(x = 250, y = 180)
+    
+    #todo lo que tenga bind es un cambio
+    placa.bind("<KeyRelease>", long_placa)
     
     #? Tipo Vehiculo titulo
     #Todo saqué el listbox del frame ya que no es necesario y ayuda a acomodarlo mejor
-    tit_tcarro = Label(p_citas, text= 'Tipo de Vehiculo', font = ("Times New Roman", 13, "bold"))
-    tit_tcarro.place(x = 10, y = 260)
+    tit_tcarro = Label(p_citas, text= 'Tipo de Vehiculo:', font = ("Times New Roman", 13, "bold"))
+    tit_tcarro.place(x = 10, y = 230)
     
     #TODO Lista de posibles vehículos y lugar en la lista
     vehiculos = ["Automovil particular y caraga liviana (menor o igual a 3500 kg)", "Automovil particular y de carga liviana (mayor a 3500 kg pero menor a 8000 kg)", "Vehículo de carga pesada y cabezales (mayor o igual a 8000 kg)", "Taxi", "Autobús, bus o microbús", "Motocicleta", "Equipo especial de obras", "Equipo especial agrícola (maquinaria agrícola)"]
@@ -114,22 +126,78 @@ def programar_cita():
     for vehiculo in vehiculos:
         listbox_vehiculo.insert(elemento, vehiculo)
         elemento += 1
-    listbox_vehiculo.place(x = 150, y = 260)
+    listbox_vehiculo.place(x = 250, y = 230)
     
     #TODO poner nota para que el usuario se mueva con las flechas en el listbox
     
     #TODO de acá en adelante son cosas que añado extra
     #? Marca del vehículo
-    #? Modelo 
+    tit_marca = Label(p_citas, text = "Marca del vehículo:", font = ("Times New Roman", 13, "bold"))
+    tit_marca.place(x = 10, y = 300)
+    
+    marca = Entry(p_citas, font = ("Times New Roman", 13), width = 15)
+    marca.place(x = 250, y = 300)
+    
+    marca.bind("<KeyRelease>", long_marca)
+        
+    #? Modelo
+    tit_modelo = Label(p_citas, text = "Modelo del vehículo:", font = ("Times New Roman", 13, "bold"))
+    tit_modelo.place(x = 10, y = 350)
+    
+    modelo = Entry(p_citas, font = ("Times New Roman", 13), width = 15)
+    modelo.place(x = 250, y = 350)
+    
+    modelo.bind("<KeyRelease>", long_modelo)
+    
     #? Propietario
-    #? Teléfono
-    #? Correo
-    #? Direccion
-    #? Fecha y hora
+    tit_prop = Label(p_citas, font = ("Times New Roman", 13, "bold"), text = "Nombre del propietario:")
+    tit_prop.place(x = 10, y = 400)
+    
+    propietario = Entry(p_citas, font = ("Times New Roman", 13), width = 40)
+    propietario.place(x = 250, y = 400)
+    
+    propietario.bind("<KeyRelease>", long_prop)
 
+    #? Teléfono
+    tit_telefono = Label(p_citas, text = "Número de teléfono:", font = ("Times New Roman", 13, "bold"))
+    tit_telefono.place(x = 10, y = 450)
+    
+    telefono = Entry(p_citas, font = ("Times New Roman", 13), width = 20)
+    telefono.place(x = 250, y = 450)
+    
+    telefono.bind("<KeyRelease>", long_telefono)
+    
+    #? Correo
+    tit_correo = Label(p_citas, text = "Correo electrónico:", font = ("Times New Roman", 13, "bold"))
+    tit_correo.place(x = 10, y = 500)
+    
+    correo = Entry(p_citas, font = ("Times New Roman", 13), width = 40)
+    correo.place(x = 250, y = 500)
+    
+    #? Direccion
+    tit_direccion = Label(p_citas, text = "Dirección física:", font = ("Times New Roman", 13, "bold"))
+    tit_direccion.place(x = 10, y = 550)
+    
+    direccion = Entry(p_citas, font = ("Times New Roman", 13), width = 40)
+    direccion.place(x = 250, y = 550)
+    
+    direccion.bind("<KeyRelease>", long_direc)
+    
+    #? Fecha y hora
+    tit_fecha = Label(p_citas, text = "Elección de fecha y hora:", font = ("Times New Roman", 13, "bold"))
+    tit_fecha.place(x = 10, y = 600)
+    
+    manual = Radiobutton(p_citas, text = "Manual", font = ("Times New Roman", 13), value = 1, variable = elegida)
+    manual.place(x = 250, y = 600)
+    
+    automatico = Radiobutton(p_citas, text = "Automático", font = ("Times New Roman", 13), value  = 2, variable = elegida)
+    automatico.place(x = 350, y = 600)
+    
+    #? Botnes de guardado y de salida
+    boton_guardar_cita = Button(p_citas,text = "Guardar cita", font = ("Times New Roman", 13), width = 13, bg = "light green")
+    boton_guardar_cita.place(x = 625, y= 650)
 
     """#! Scroll bar 
->>>>>>> 1eb4ae5 (Versión 1.1)
     # Crear un Frame para contener el Listbox y el Scrollbar
     frame_scrollbar = Frame(p_citas)
     frame_scrollbar.place(x = 750, y = 50, width=250, height=400)
@@ -145,11 +213,7 @@ def programar_cita():
 
     # Agregar elementos al Listbox
     for i in range(100):
-<<<<<<< HEAD
-        listbox.insert(END, f"Elemento {i}")
-=======
         listbox.insert(END, f"Elemento {i}")"""
->>>>>>> 1eb4ae5 (Versión 1.1)
     p_citas.mainloop()
 #?============================================================= Menu Principal ===================================================================================================
 
