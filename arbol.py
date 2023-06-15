e2e7ea08b7c2e2a08fd79273b646ba94ff184531
Bonko24 <134968235+Bonko24@users.noreply.github.com>
2023-06-15 16:08:05 -0600

    arbol arreglado y funcional

--- arbol.py
+++ arbol.py
@@ -8,35 +8,35 @@ class Nodo:
         self.izquierdo = None
         self.derecho = None
 
-# Arma el árbol y define los nodos izuqierdo y derechos
+# Arma el árbol y define los nodos izquierdo y derecho
 class Arbol:
     def __init__(self):
         self.raiz = None
     
-    # Define el primer elementoi del árbol, su raiz y, en caso de estar definida, se mueve a los nodos
+    # Define el primer elemento del árbol, su raíz y, en caso de estar definida, se mueve a los nodos
     def agregar(self, lista):
-        if self.raiz == None:
+        if self.raiz is None:
             self.raiz = Nodo(lista)
         else:
-            self._agregar_aux(lista, self.raiz)
+            self.agregar_aux(lista, self.raiz)
     
-    # Define los nodos del arbol
-    def _agregar_aux(self, lista, nodo_actual):
+    # Define los nodos del árbol
+    def agregar_aux(self, lista, nodo_actual):
         if lista < nodo_actual.lista:
-            if nodo_actual.izquierdo == None:
-                nodo_actual.lista = Nodo(lista)
+            if nodo_actual.izquierdo is None:
+                nodo_actual.izquierdo = Nodo(lista)
             else:
-                self._agregar_aux(lista, nodo_actual.izquierdo)
-        elif nodo_actual.derecho == None:
-            if self.derecho == None:
-                self.derecho = Nodo(lista)
+                self.agregar_aux(lista, nodo_actual.izquierdo)
+        else:
+            if nodo_actual.derecho is None:
+                nodo_actual.derecho = Nodo(lista)
             else:
-                self._agregar_aux(lista, nodo_actual.derecho)
+                self.agregar_aux(lista, nodo_actual.derecho)
                 
-    # guardar datos en un archivo .dat
+    # Guardar datos en un archivo .dat
     # Así se almacenarán las citas
     def guardar_datos(self, nodo, archivo):
-        if nodo != None:
+        if nodo is not None:
             self.guardar_datos(nodo.izquierdo, archivo)
             pickle.dump(nodo.lista, archivo)
             self.guardar_datos(nodo.derecho, archivo)
